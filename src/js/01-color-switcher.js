615c980a172a3dc5fc2ca91e12792d352203bdb6
Olena Shavala <olena.shvl@gmail.com>
2023-06-04 10:45:05 +0300

    first fixation

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+let intervalId;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeBackgroundColor() {
+  document.body.style.background = getRandomHexColor();
+}
+
+function startColorChanging() {
+  intervalId = setInterval(changeBackgroundColor, 1000);
+  startButton.disabled = true;
+  stopButton.disabled = false;
+}
+
+function stopColorChanging() {
+  clearInterval(intervalId);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+}
+
+startButton.onclick = startColorChanging;
+stopButton.onclick = stopColorChanging;
